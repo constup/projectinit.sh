a87d259e90eaf97d8f5a98834ef8586bc231574f
constup <16638173+constup@users.noreply.github.com>
2025-02-14 13:58:22 +0100

    Improved code readability.

--- src/php/symfony/create_project.sh
+++ src/php/symfony/create_project.sh
@@ -23,3 +23,66 @@ create_symfony_project () {
       ;;
   esac
 }
+
+configure_symfony_project() {
+  cd "${project_root_dir}" || exit 1;
+  local symfony_version
+  echo "Select Symfony version. Note: the latest minor version will be used."
+  local sym_ver=("Symfony 5" "Symfony 6" "Symfony 7" "Custom")
+  select sv in "${sym_ver[@]}"; do
+    case $sv in
+      "Symfony 5" )
+        echo ""
+        echo "Symfony 5.4 will be used."
+        echo "  - Release date: November 2021."
+        echo "  - End of bug fixes: November 2024."
+        echo "  - End of security fixes: February 2029."
+        echo "  - Requires PHP 7.2.5 or higher."
+        echo ""
+        symfony_version="5.4.x"
+        break;;
+      "Symfony 6" )
+        echo ""
+        echo "Symfony 6.4 will be used."
+        echo "  - Release date: November 2023."
+        echo "  - End of bug fixes: November 2026."
+        echo "  - End of security fixes: February 2027."
+        echo "  - Requires PHP 8.1.0 or higher."
+        echo ""
+        symfony_version="6.4.x"
+        break;;
+      "Symfony 7" )
+        echo ""
+        echo "Symfony 7.2 will be used."
+        echo "  - Release date: November 2024."
+        echo "  - End of support: July 2025."
+        echo "  - Requires PHP 8.2.0 or higher."
+        echo ""
+        symfony_version="7.2.x"
+        break;;
+      "Custom" )
+        echo ""
+        echo "Manually enter your desired Symfony version (format: 7.1):"
+        read -r -e symfony_version
+        symfony_version="${symfony_version}.x"
+        break;;
+    esac
+  done
+
+  echo ""
+  echo "Choose application type:"
+  local app_type
+  local app_type_options=("Web application with frontend" "CLI or API application")
+  select ato in "${app_type_options[@]}"; do
+    case $ato in
+      "Web application with frontend" )
+        app_type="full"
+        break;;
+      "CLI or API application" )
+        app_type="api"
+        break;;
+    esac
+  done
+
+  create_symfony_project $symfony_version $app_type "composer"
+}
