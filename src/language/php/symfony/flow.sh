9500db75a007d097c7bf50773ef277120328b0a5
constup <16638173+constup@users.noreply.github.com>
2025-03-17 14:20:26 +0100

    php.ini
    - added custom php.ini to Symfony and Composer

--- src/language/php/symfony/flow.sh
+++ src/language/php/symfony/flow.sh
@@ -55,6 +55,9 @@ run_symfony_flow_docker() {
   cp -f "${tool_dir}/src/language/php/symfony/container/docker/doc/PROJECTINIT_README_v1.adoc" "${project_root_dir}/PROJECTINIT_README.adoc"
   # shellcheck source=../../../container/docker/dev.sh
   source "${tool_dir}/src/container/docker/dev.sh"
+  # shellcheck source=../tools/php_ini/php_ini.sh
+  source "${tool_dir}/src/language/php/tools/php_ini/php_ini.sh"
+  configure_php_ini_docker_dev
   build_dev
 
   # shellcheck source=../../../git/git.sh
@@ -63,4 +66,5 @@ run_symfony_flow_docker() {
   setup_prod_dockerfile
   setup_prod_entrypoint
   setup_prod_compose
+  configure_php_ini_docker_prod
 }
