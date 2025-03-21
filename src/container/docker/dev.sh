107e799ff567a187d024d6f188f35277e73cbc1b
constup <16638173+constup@users.noreply.github.com>
2025-03-21 19:40:15 +0100

    .env.docker
    - .env.docker directly added to docker compose.yaml

--- src/container/docker/dev.sh
+++ src/container/docker/dev.sh
@@ -8,7 +8,7 @@ build_dev() {
   echo ""
   echo "Building Dev container..."
   cd "${project_root_dir}" || exit 1
-  docker compose --env-file .env.docker build
+  docker compose build
   echo "  Dev container built..."
 }
 
--- src/container/docker/dot_env_docker/docker_dot_env.sh
+++ src/container/docker/dot_env_docker/docker_dot_env.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 add_user_and_group_ids() {
-  if [ ! -f "${project_root_dir}/.env.docker" ]; then
-    touch "${project_root_dir}/.env.docker"
+  if [ ! -d "${project_root_dir}/projectinit_docker" ]; then
+    mkdir -p "${project_root_dir}/projectinit_docker"
+  fi
+  if [ ! -f "${project_root_dir}/projectinit_docker/.env.docker" ]; then
+    touch "${project_root_dir}/projectinit_docker/.env.docker"
   fi
   local user_id
   local group_id
