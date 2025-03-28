6e8da3ac2d474f5027eb72b7ec09c4e31dba1a79
constup <16638173+constup@users.noreply.github.com>
2025-03-28 19:15:54 +0100

    Postgres
    - In-project documentation
    
    Documentation
    
    - fixes for healthcheck documentation

--- src/tools/documentation/documentation.sh
+++ src/tools/documentation/documentation.sh
@@ -10,6 +10,14 @@ setup_documentation() {
 
 setup_database_documentation() {
   case $projectinit_database_type in
+    "pgsql" )
+      cp -f "${tool_dir}/src/database/pgsql/container/docker/v1/template/documentation/index.adoc" "${project_root_dir}/doc/projectinit/postgresql.adoc"
+      case $projectinit_container_type in
+        "docker" )
+          perl -pi -e "s/~~~implemented configuration~~~/$(<"${tool_dir}/src/database/pgsql/container/docker/v1/template/documentation/implemented_configuration.adoc" perl -pe 's/([\/\& \t])/\\$1/g')/g" "${project_root_dir}/doc/projectinit/postgresql.adoc"
+          ;;
+      esac
+      ;;
     "percona" )
       cp -f "${tool_dir}/src/database/percona/v1/template/documentation/index.adoc" "${project_root_dir}/doc/projectinit/percona_mysql.adoc"
       case $projectinit_container_type in
